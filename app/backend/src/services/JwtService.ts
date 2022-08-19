d9b7c320671eb525f33a6ae8bd3e34e3da6ba45f
Raphael <rafa.ramos_22@hotmail.com>
2022-08-18 22:24:57 -0300

    feat: adicionando loginService

--- app/backend/src/services/JwtService.ts
+++ app/backend/src/services/JwtService.ts
@@ -1,22 +1,22 @@
 import * as jwt from 'jsonwebtoken';
 import { SignOptions } from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
-import { IUser } from '../interfaces/IUser';
+import  ILogin  from '../interfaces/ILogin';
 
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
 
 export default class Jwt {
-  static createToken(user: IUser): string {
-  const { id, email } = user;
+  static createToken(user: ILogin): string {
+  const { email } = user;
 
   const jwtConfig: SignOptions = {
     expiresIn: '7d',
     algorithm: 'HS256',
   };
 
-  const token = jwt.sign({ id, email }, JWT_SECRET, jwtConfig);
+  const token = jwt.sign({ email }, JWT_SECRET, jwtConfig);
   return token;
   }
 };
--- app/backend/src/services/loginService.ts
+++ app/backend/src/services/loginService.ts
@@ -1,10 +1,11 @@
-import User from 'src/database/models/User';
+import Jwt from './JwtService';
 import ILogin from '../interfaces/ILogin';
 
-
-
-export default class LoginService implements Login {
-  constructor(private model = User) {
-    this.model = model;
+export default class Login{
+  static async Login(login:ILogin) {
+     const token = await Jwt.createToken(login);
+    return token;
   }
 }
+
+ 
