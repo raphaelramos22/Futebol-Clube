d34a03e9ac4dc7757b2c0708c7f81c000c422fe8
Raphael <rafa.ramos_22@hotmail.com>
2022-08-19 15:37:47 -0300

    feat: criando validaçao que verifica se o email do usuario é um email correto

--- app/backend/src/services/loginService.ts
+++ app/backend/src/services/loginService.ts
@@ -1,19 +1,30 @@
 import Jwt from './JwtService';
 import ILogin from '../interfaces/ILogin';
+import User from '../database/models/User';
 
 
 export default class Login{
   static async Login(login:ILogin) {
   const {email, password} = login
+  
   if(!email || !password){
     const e = new Error();
     e.name = 'badRequest';
     e.message = 'All fields must be filled';
     throw e;
   }
+  const user = await User.findOne({ where: { email: login.email } });
+  if(!user){
+    const e = new Error();
+    e.name = 'UnauthorizedError';
+    e.message = 'Incorrect email or password';
+    throw e;
+  }
+
      const token = await Jwt.createToken(login);
     return token;
   }
+
 }
 
  
