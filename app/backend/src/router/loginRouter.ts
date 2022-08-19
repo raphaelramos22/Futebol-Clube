737236e9169edfeabe975e60352b8b245856302b
Raphael <rafa.ramos_22@hotmail.com>
2022-08-19 14:24:34 -0300

    limpando consolelog se ultilidade

--- app/backend/src/router/loginRouter.ts
+++ app/backend/src/router/loginRouter.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import LoginController from '../controller/loginCotroller'
 
 const router = Router();
-console.log('cheguei');
 
 router.post('/login', LoginController.login);
 
