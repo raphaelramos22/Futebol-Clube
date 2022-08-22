873ee11d8fcda1c0f318936af6d6dfe328dcb8e2
Raphael <rafa.ramos_22@hotmail.com>
2022-08-22 16:25:43 -0300

    feat: adicionando funçao getById na class Teams do teamSevice que busca o time atravez do id

--- app/backend/src/services/teamService.ts
+++ app/backend/src/services/teamService.ts
@@ -6,5 +6,9 @@ export default class Teams{
   const allTeams = await Team.findAll();
   return allTeams
   }
+  static async getById(id: number): Promise<Team | null > {
+    const team = await Team.findOne({ where: { id } });
+    return team;
+  }
 }
  
