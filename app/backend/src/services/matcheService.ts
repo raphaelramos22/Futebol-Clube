d2922666215703f3032464f86a49202810a9852a
Raphael <rafa.ramos_22@hotmail.com>
2022-08-22 17:52:12 -0300

    fix: corrigindo erro de ortografia

--- app/backend/src/services/matcheService.ts
+++ app/backend/src/services/matcheService.ts
@@ -1,9 +1,9 @@
 import Teams from '../database/models/Team';
-import matches from '../database/models/Matche';
+import Matche from '../database/models/Matche';
 
 export default class Matches{
   static async getAll(): Promise<Matches[]> {
-    const allMatches = await matches.findAll({ include: [
+    const allMatches = await Matche.findAll({ include: [
       { model: Teams, as: 'teamHome', attributes: ['teamName'] },
       { model: Teams, as: 'teamAway', attributes: ['teamName'] },
     ] });
