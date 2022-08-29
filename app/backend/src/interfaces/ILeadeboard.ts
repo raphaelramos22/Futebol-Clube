5e3f327ddb31b908e153341822cf4e4765ae0faf
Raphael <rafa.ramos_22@hotmail.com>
2022-08-29 20:05:08 -0300

    feat: interfaces de IleaderBord e IteamHome

---
+++ app/backend/src/interfaces/ILeadeboard.ts
@@ -0,0 +1,20 @@
+import matches from "./IMatche";
+
+export interface ILeaderBoard {
+  name: string,
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalance: number,
+  efficiency: number,
+}
+
+export interface ITeamHome {
+  id?: number,
+  teamName: string,
+  homeTeam: matches[],
+}
